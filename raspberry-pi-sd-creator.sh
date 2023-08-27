40484b9f1fbed2e6668a2e8daa5785e9d0bc783b
Jens Ackou <shackofnoreturn@gmail.com>
2023-08-27 22:35:56 +0200

    Added help option

--- raspberry-pi-sd-creator.sh
+++ raspberry-pi-sd-creator.sh
@@ -1,2 +1,35 @@
 #!/bin/bash
+
+############################################################
+# Help                                                     #
+############################################################
+Help() {
+   # Display all available options
+   echo "Add description of the script functions here."
+   echo
+   echo "Syntax: scriptTemplate [-g|h|v|V]"
+   echo "options:"
+   echo "g     Print the GPL license notification."
+   echo "h     Print this Help."
+   echo "v     Verbose mode."
+   echo "V     Print software version and exit."
+   echo
+}
+
+############################################################
+# Process the input options                                #
+############################################################
+# Get the options
+while getopts ":h" option 
+do
+    case $option in
+        h) # display Help
+            echo "HELP"
+            Help
+            exit
+            ;;
+    esac
+    shift
+done
+
 echo "Hello World"
