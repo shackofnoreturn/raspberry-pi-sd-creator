4116ff62bdb0c65bd882691506d36881f7457871
Jens Ackou <shackofnoreturn@gmail.com>
2023-08-31 16:10:44 +0200

    Setting hostname and restart system

--- setup.sh
+++ setup.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+echo "Running post installation script."
+
+# Network configuration
+sudo hostname ${host}
+
+# Reboot after all changes above complete
+echo "Restarting to apply changes." 
+echo "After that,  run ssh pi@${host}.local"
+/sbin/shutdown -r now
