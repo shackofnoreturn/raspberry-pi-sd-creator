99613130514270e227507ca8094b89fb2e7eb5fc
Jens Ackou <shackofnoreturn@gmail.com>
2023-09-04 20:02:25 +0200

    Changing hostname in post setup

--- setup.sh
+++ setup.sh
@@ -2,9 +2,15 @@
 echo "Running post installation script."
 
 # Network configuration
-sudo hostname ${host}
+echo "Current hostname: "
+sudo hostname
+
+echo "Changing hostname to ${host}"
+sudo sed -i 's/raspberrypi/${host}/g' /etc/hostname
+sudo sed -i 's/raspberrypi/${host}/g' /etc/hosts
 
 # Reboot after all changes above complete
-echo "Restarting to apply changes." 
+echo "Restarting to apply changes in 10 seconds." 
 echo "After that,  run ssh pi@${host}.local"
+sleep 10
 /sbin/shutdown -r now
