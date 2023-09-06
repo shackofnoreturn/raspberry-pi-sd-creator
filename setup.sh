760bc05ee07778a7f47757ed5ed75452e53e4af2
Jens Ackou <shackofnoreturn@gmail.com>
2023-09-06 16:11:15 +0200

    Change pi user password ASAP to prevent further issues

--- setup.sh
+++ setup.sh
@@ -9,6 +9,10 @@ echo "Changing hostname to ${host}"
 sudo sed -i 's/raspberrypi/${host}/g' /etc/hostname
 sudo sed -i 's/raspberrypi/${host}/g' /etc/hosts
 
+# Security
+echo "Changing pi user password:"
+passwd pi
+
 # Reboot after all changes above complete
 echo "Restarting to apply changes in 10 seconds." 
 echo "After that,  run ssh pi@${host}.local"
